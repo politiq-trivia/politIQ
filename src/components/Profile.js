f29fc3cb1374362031b53ba6aea52e02e3fb5cd6
Hannah Werman <hanna.w28@gmail.com>
2018-10-16 17:05:36 +0100

    password reset and forget page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const Profile = () =>
-  <div>
-    <h1>Profile</h1>
-  </div>
+import AuthUserContext from './AuthUserContext';
+import { PasswordForgetForm } from './PasswordForget';
+import PasswordChangeForm from './PasswordChange';
 
-export default Profile;
+const ProfilePage = () =>
+  <AuthUserContext.Consumer>
+    {authUser =>
+      <div>
+        <h1> Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    }
+  </AuthUserContext.Consumer>
+
+export default ProfilePage;
