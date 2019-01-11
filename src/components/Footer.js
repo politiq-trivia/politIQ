681bd1797c5057700e5bc3ce2b7ba032def83a58
Hannah Werman <hanna.w28@gmail.com>
2019-01-11 00:26:59 -0600

    fixed footer link and no match home button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Link } from 'react-router-dom'
 import * as routes from '../constants/routes';
+import { db } from '../firebase';
 
 import {
   FacebookShareButton,
@@ -39,68 +40,99 @@ const isFixed = () => {
 
 }
 
-// const style = {
-//   footerStyle: {
-//     position: isFixed()
-//   }
-// }
+
+// let mostRecentQuizURL;
 
 
-const Footer = ({ signedInUser }) =>
-  <div className="footer" style={isFixed()}>
-    { signedInUser === ""
-      ? <div className="description">
-          <h3>About Us</h3>
-          <p>In an effort to combat polarization and improve the dialogue around politics, I created this site as a platform for generating a common consensus of current events and political realities through a friendly competition. I mean, everyone loves a fun and informative game, right?</p>
-        <Link to={routes.ABOUT}>Read More</Link>
-        </div>
-      : <div className="description">
-          <h3>Find some news we should include?</h3>
-          <p>Increase your score by clicking the link below to submit your own question. We'll review it and, if accepted, we'll add it to the quiz. Make sure to include a reliable source for your information!</p>
-          <Link to={routes.SUBMIT_QUESTION}>Submit a Question Now</Link>
-        </div>
+
+
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mostRecentQuizURL: ""
     }
+  }
+
+  componentDidMount() {
+    this.getMostRecentQuizId()
+  }
+
+  getMostRecentQuizId = async () => {
+    await db.getQuizzes()
+      .then(response => {
+        if (response.val() !== null) {
+          const data = response.val();
+          const dateArray = Object.keys(data);
+          const mostRecent = dateArray[dateArray.length-1]
+  
+          this.setState({
+            mostRecentQuizURL: "quiz/" + mostRecent
+          })
+
+        }
+      })
+  }
+
+  render() {
+    return (
+      <div className="footer" style={isFixed()}>
+        { this.props.signedInUser === ""
+          ? <div className="description">
+              <h3>About Us</h3>
+              <p>In an effort to combat polarization and improve the dialogue around politics, I created this site as a platform for generating a common consensus of current events and political realities through a friendly competition. I mean, everyone loves a fun and informative game, right?</p>
+            <Link to={routes.ABOUT}>Read More</Link>
+            </div>
+          : <div className="description">
+              <h3>Find some news we should include?</h3>
+              <p>Increase your score by clicking the link below to submit your own question. We'll review it and, if accepted, we'll add it to the quiz. Make sure to include a reliable source for your information!</p>
+              <Link to={routes.SUBMIT_QUESTION}>Submit a Question Now</Link>
+            </div>
+        }
 
 
-    <div className="links">
-      <h3>Quick Links</h3>
-      <Link to={routes.HOME} style={{ color: 'white', textDecoration: 'none', marginTop: '1vh', display: 'block' }}>Home</Link>
-      <Link to={routes.ABOUT} style={{ color: 'white', textDecoration: 'none', marginTop: '1vh', display: 'block' }}>About</Link>
-      <Link to={routes.SIGN_UP} style={{ color: 'white', textDecoration: 'none', marginTop: '1vh', display: 'block' }}>Sign Up</Link>
-    </div>
+        <div className="links">
+          <h3>Quick Links</h3>
+          <Link to={routes.HOME} style={{ color: 'white', textDecoration: 'none', marginTop: '1vh', display: 'block' }}>Home</Link>
+          <Link to={routes.ABOUT} style={{ color: 'white', textDecoration: 'none', marginTop: '1vh', display: 'block' }}>About</Link>
+          <Link to={routes.SIGN_UP} style={{ color: 'white', textDecoration: 'none', marginTop: '1vh', display: 'block' }}>Sign Up</Link>
+        </div>
 
-    <div className="sharing">
-      <h3>Share on Social Media</h3>
+        <div className="sharing">
+          <h3>Share on Social Media</h3>
 
-      <div className="socials" style={{ display: 'flex', marginTop: '1vh' }}>
+          <div className="socials" style={{ display: 'flex', marginTop: '1vh' }}>
 
-        <FacebookShareButton url={ getHref() } className="shareable">
-          <FacebookIcon round={true} size={32} />
-        </FacebookShareButton>
-        <GooglePlusShareButton url={ getHref() } className="shareable">
-          <GooglePlusIcon round={true} size={32} />
-        </GooglePlusShareButton>
-        <LinkedinShareButton url={ getHref() } className="shareable">
-          <LinkedinIcon round={true} size={32}/>
-        </LinkedinShareButton>
-        <TwitterShareButton url={ getHref() } className="shareable">
-          <TwitterIcon round={true} size={32} />
-        </TwitterShareButton>
-        <WhatsappShareButton url={ getHref() } className="shareable">
-          <WhatsappIcon round={true} size={32} />
-        </WhatsappShareButton>
-        <EmailShareButton url={ getHref() } className="shareable">
-          <EmailIcon round={true} size={32} />
-        </EmailShareButton>
+            <FacebookShareButton url={ getHref() } className="shareable">
+              <FacebookIcon round={true} size={32} />
+            </FacebookShareButton>
+            <GooglePlusShareButton url={ getHref() } className="shareable">
+              <GooglePlusIcon round={true} size={32} />
+            </GooglePlusShareButton>
+            <LinkedinShareButton url={ getHref() } className="shareable">
+              <LinkedinIcon round={true} size={32}/>
+            </LinkedinShareButton>
+            <TwitterShareButton url={ getHref() } className="shareable">
+              <TwitterIcon round={true} size={32} />
+            </TwitterShareButton>
+            <WhatsappShareButton url={ getHref() } className="shareable">
+              <WhatsappIcon round={true} size={32} />
+            </WhatsappShareButton>
+            <EmailShareButton url={ getHref() } className="shareable">
+              <EmailIcon round={true} size={32} />
+            </EmailShareButton>
 
+          </div>
+
+        </div>
+        <div className="bottom-nav">
+          <Link to={this.state.mostRecentQuizURL} id="play">Play Now</Link>
+          <Link to="/leaderboard">Leaderboard</Link>
+          <Link to="/about">About PolitIQ</Link>
+        </div>
       </div>
-
-    </div>
-    <div className="bottom-nav">
-      <Link to="/play-game" id="play">Play Now</Link>
-      <Link to="/leaderboard">Leaderboard</Link>
-      <Link to="/about">About PolitIQ</Link>
-    </div>
-  </div>
+    )
+  }
+}
 
 export default Footer;
