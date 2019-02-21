5794e8bab55c4618dbe06ecdc4cfa152d16f49ad
Hannah Werman <hanna.w28@gmail.com>
2019-02-21 13:43:01 +0700

    updated politiq and submitted/contested scores on profile page

--- src/components/Leaderboard/UserScoreboard.js
+++ src/components/Leaderboard/UserScoreboard.js
@@ -11,6 +11,7 @@ class UserScoreboard extends Component {
       data: {},
       weeklyScore: 0,
       submittedScore: 0,
+      recentSubmittedScores: 0,
       moneyWon: '$0',
       politIQ: 0,
     }
@@ -38,13 +39,13 @@ class UserScoreboard extends Component {
         if (data === null) {
           this.setState({
             monthlyScore: 0,
-            // allTimeScore: 0,
             submittedScore: 0,
           })
           return;
         }
 
         let submitted = [];
+        let recentSubmittedScores;
         const quizDates = Object.keys(data)
         if (quizDates[quizDates.length -1] === 'submitted') {
           submitted = data["submitted"]
@@ -52,43 +53,53 @@ class UserScoreboard extends Component {
         }
 
         let weeklyScore = 0;
-        // let allTimeScore = 0;
         let submittedScore = 0;
 
         for (let i = 0; i < quizDates.length; i++) {
           if (quizDates[i] > moment().startOf('week').format('YYYY-MM-DD')) {
             weeklyScore += data[quizDates[i]]
           }
-          // allTimeScore += data[quizDates[i]]
         }
 
         if (submitted !== []) {
           const dates = Object.keys(submitted)
-          for (let j = 0; j < dates.length; j++) {
-              submittedScore += 1
-            }
+          const recentDates = dates.filter(date => date > moment().startOf('month').format('YYYY-MM-DDTHH:mm'))
+          let recentScores = [];
+          for (let i = 0; i < recentDates.length; i++) {
+            const score = submitted[recentDates[i]]
+            recentScores.push(score)
+          }
+          const scores = Object.values(submitted)
+          submittedScore = scores.reduce((a, b) => a + b)
+          recentSubmittedScores = recentScores.reduce((a, b) => a + b)
         }
 
         this.setState({
           weeklyScore,
-          // allTimeScore,
           submittedScore,
+          recentSubmittedScores,
         })
       })
   }
 
   getMyPolitIQ = async (uid, timeframe) => {
-    const iq = await getPolitIQ(uid, timeframe)
-    this.setState({
-      politIQ: iq,
-    })
+    let iq = await getPolitIQ(uid, timeframe)
+    if (isNaN(iq)) {
+      this.setState({
+        politIQ: 0
+      })
+    } else {
+      this.setState({
+        politIQ: iq,
+      })
+    }
   }
 
   render() {
     return (
       <div className="small-scoreboardHolder user-scoreboard-public">
         <h2>{this.props.public ? `${this.props.name}'s`: "My"} Scores</h2>
-        <div className="userScore politIQ">PolitIQ<span className="score reg-score">{this.state.politIQ + this.state.submittedScore}</span></div>
+        <div className="userScore politIQ">PolitIQ<span className="score reg-score">{this.state.politIQ + this.state.recentSubmittedScores}</span></div>
         <div className="small-scoreboard">
           <div className="userScore" style={{ borderLeft: 'none' }}>Weekly Score<span className="score reg-score">{this.state.weeklyScore}</span></div>
           <div className="userScore" id="submittedQScore">Submitted & Contested Q Score<span className="score">{this.state.submittedScore}</span></div>
