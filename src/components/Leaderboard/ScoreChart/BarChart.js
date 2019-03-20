409274bd5ea82d3cf8b4941bec998afe21762816
Hannah Werman <hanna.w28@gmail.com>
2019-03-20 20:52:36 +0700

    fixed chart axis labels and added loading gif

--- src/components/Leaderboard/ScoreChart/BarChart.js
+++ src/components/Leaderboard/ScoreChart/BarChart.js
@@ -3,11 +3,8 @@ import moment from 'moment';
 import { scaleBand, scaleLinear } from 'd3-scale';
 import * as d3 from 'd3';
 import { Element } from 'react-faux-dom';
-import Button from '@material-ui/core/Button';
 import { db } from '../../../firebase';
 
-import Axis from './Axis';
-import Bars from './Bars';
 import ResponsiveChart from './ChartWrapper';
 import "./Axis.css";
 
@@ -19,6 +16,7 @@ class BarChart extends Component {
           Republican: 0,
           Independent: 0,
           containerWidth: 600,
+          loading: true,
         }
         this.xScale = scaleLinear();
 
@@ -65,6 +63,11 @@ class BarChart extends Component {
             this.getDemScores(data, 'Democrat', timeFrame)
             this.getDemScores(data, 'Republican', timeFrame)
             this.getDemScores(data, 'Independent', timeFrame)
+          })
+          .then(() => {
+            this.setState({
+              loading: false
+            })
           })
       }
     
@@ -130,18 +133,11 @@ class BarChart extends Component {
     }
 
     plot(chart, width, height, margin) {
-      console.log(this.state)
       const data = [
-        { party: 'Democrat', score: ((this.state.Democrat / this.state.Democratlength)  * 100).toString() },
-        { party: 'Republican', score: (this.state.Republican / this.state.Republicanlength) * 100 },
-        { party: 'Independent', score: (this.state.Independent / this.state.Independentlength) * 100 },
+        { party: 'D', score: ((this.state.Democrat / this.state.Democratlength)  * 100).toString() },
+        { party: 'R', score: (this.state.Republican / this.state.Republicanlength) * 100 },
+        { party: 'I', score: (this.state.Independent / this.state.Independentlength) * 100 },
       ]
-
-      // const data = [
-      //   { party: 'Democrat', score: 15 },
-      //   { party: 'Republican', score: 20 },
-      //   { party: 'Independent', score: 25 },
-      // ]
 
       const maxValue = Math.max(...data.map(d => d.score));
 
@@ -153,7 +149,6 @@ class BarChart extends Component {
       const xScale = this.xScale
         .domain([0, maxValue])
         .range([0, width])
-        // .translate(`translate(0, ${height - margin.bottom})`)
 
       const colorScale = d3.scaleOrdinal(['red', 'blue', 'green'])
 
@@ -195,15 +190,6 @@ class BarChart extends Component {
         .attr('dy', (d, i) => yScale(d.party) + (yScale.bandwidth() / 2) + 6)
         .text(d => d.score)
 
-      // const xAxis = d3.axisBottom()
-      //     .scale(xScale);
-      
-      // chart.append('g')
-      //     .classed('x axis', true)
-      //     .attr('transform', `translate(0, ${height})`)
-      //     .ticks()
-      //     .call(xAxis)
-
       const yAxis = d3.axisRight()
           .ticks(3)
           .scale(yScale)
@@ -239,56 +225,13 @@ class BarChart extends Component {
 
 
     render () {
-        // const margin = { top: 10, right: 20, bottom: 60, left: 100 };
-        // const width = this.state.containerWidth;
-        // const height = 250;
-
-
-
-
-
-        // const yProps = {
-        //   orient: 'Left',
-        //   scale: yScale,
-        //   translate: `translate(${margin.left}, 0)`,
-        //   tickSize: width - margin.left - margin.right,
-        // }
-
-        // const xProps = {
-        //   orient: 'Bottom',
-        //   scale: xScale,
-        //   ,
-        //   tickSize: height - margin.top - margin.bottom,
-        // }
         
         return (
           <>
-            {this.drawChart()}
-            {/* { maxValue 
-              ?
-               <div className="chartHolder">
-                  <div>
-                    <p style={{  marginTop: '6vh', width: '100%', fontWeight: 'bold' }}>Average PolitIQ</p>
-                  </div>
-                  <svg id="chart" width={width} height={height}>
-                    <Axis {...yProps} />
-                    <Axis {...xProps} />
-                    <Bars 
-                      xScale={xScale}
-                      yScale={yScale}
-                      margin={margin}
-                      data={data}
-                      maxValue={maxValue}
-                      height={height}
-                      width={width}
-                    />
-                  </svg>
-                </div>
-             : <>
-                <h3>There are no scores logged for this {this.props.timeFrame}!</h3>
-                <Button variant="contained" color="primary">Click Here to Take Today's Quiz</Button>
-              </>
-            } */}
+            {this.state.loading 
+              ? <p>Loading...</p>
+              : <>{this.drawChart()}</>
+            }
           </>
         )
     }
