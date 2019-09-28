cd00539fcd6fd18dd397dad3a882361e87b7c990
Hannah Werman <hanna.w28@gmail.com>
2019-09-28 15:45:34 -0400

    WIP: context

--- src/components/QuizEngine/QuizArchive.js
+++ src/components/QuizEngine/QuizArchive.js
@@ -19,7 +19,7 @@ import Modal from '@material-ui/core/Modal';
 import TablePaginationActions from './TablePaginationActions';
 import bg from '../StaticPages/politiq-bg2.jpg';
 import AuthUserContext from '../Auth/AuthUserContext';
-import { QuizContext }from './QuizContext';
+import { QuizContext } from './QuizContext';
 import * as routes from '../../constants/routes';
 
 import loadingGif from '../../loadingGif.gif';
@@ -31,8 +31,8 @@ class QuizArchiveBase extends Component {
     super(props);
 
     this.state = {
-      dateArray: [],
-      titleArray: [],
+      // dateArray: [],
+      // titleArray: [],
       scoreObject: {},
       rowsPerPage: 10,
       page: 0,
@@ -44,7 +44,8 @@ class QuizArchiveBase extends Component {
   }
 
   componentDidMount = () => {
-    console.log('componentDidMount')
+    console.log('componentDidMount');
+    console.log(this.props.quizzes, 'quizzes');
     this.createMonthOptionsArray();
     // check for logged in user so that it can check if the user already ahas a score for that quiz
     if (this.props.authUser) {
@@ -56,7 +57,7 @@ class QuizArchiveBase extends Component {
       this.getTheLoggedInUsersScores();
     }
     const month = moment().startOf('month').format('YYYY-MM-DDTHH:mm');
-    
+
     this.setState({
       selectedMonth: month,
       displayMonth: month,
@@ -65,20 +66,21 @@ class QuizArchiveBase extends Component {
     if (this.props.quizzes.length === 0) {
       this.setState({
         loading: true,
-      })
+      });
     }
 
     if (this.props.authUser && this.props.quizzes.length > 0) {
-      this.setState({
-        loading: false,
-      })
-      this.getQuizzesFromDb();
+      // this.setState({
+      //   loading: false,
+      // });
       this.checkLoaded();
     }
+    this.getQuizzesFromDb();
+
   }
 
   componentDidUpdate(prevProps) {
-    console.log('componentDidUpdate')
+    console.log('componentDidUpdate');
     if (prevProps !== this.props) {
       if (this.props.authUser) {
         this.setState({
@@ -87,15 +89,14 @@ class QuizArchiveBase extends Component {
         this.getQuizzesFromDb();
         this.getTheLoggedInUsersScores();
       }
-
     }
 
     if (this.props.authUser && this.props.quizzes.length > 0) {
-      console.log('IF CALLE')
-      this.setState({
-        loading: false,
-        noQuizzes: false,
-      })
+      console.log('IF CALLE');
+      // this.setState({
+      //   loading: false,
+      //   noQuizzes: false,
+      // });
     }
   }
 
@@ -126,14 +127,15 @@ class QuizArchiveBase extends Component {
   // user should be able to select a month for a dropdown and then retrieve the quizzes for that
   // month. the default on page load will be the current month
   getQuizzesFromDb = async () => {
-    console.log('get quizzes from db called')
+    console.log('get quizzes from db called');
     // instead of making a db call here, get the quizzes from local storage
     const data = this.props.quizzes; // eslint-disable-line no-undef
-
+    console.log(data, 'this is data')
     const allDates = Object.keys(data);
 
     const dateArray = allDates.filter((date) => this.dateFilter(date));
-    if (dateArray.length === 0) {
+    console.log(dateArray, 'this is dateArray')
+    if (Object.keys(data).length === 0 && this.state.dateArray.length === 0) {
       this.setState({
         // noQuizzes: true,
         // loading: false,
@@ -150,6 +152,7 @@ class QuizArchiveBase extends Component {
       const title = data[date]['quiz-title'];
       titleArray.push(title);
     }
+    console.log({dateArray, titleArray})
     this.setState({
       dateArray: dateArray.reverse(),
       titleArray: titleArray.reverse(),
@@ -165,7 +168,7 @@ class QuizArchiveBase extends Component {
   }
 
   getTheLoggedInUsersScores = () => {
-    console.log('get logged in users scores')
+    console.log('get logged in users scores');
     const uid = this.state.signedInUser;
     if (uid === '') {
       return;
@@ -177,27 +180,26 @@ class QuizArchiveBase extends Component {
       return;
     }
 
-    console.log(scores, 'this is scores')
+    console.log(scores, 'this is scores');
 
     this.setState({
       scoreObject: scores,
-      loading: false,
     });
   }
 
   checkLoaded = () => {
-    window.setTimeout(() => {
-      this.toggleLoaded()
+    window.setTimeout(() => { // eslint-disable-line no-undef
+      this.toggleLoaded();
     }, 5000);
   }
 
   toggleLoaded = () => {
     if (this.props.quizzes.length === 0) {
-      console.log('toggleloaded called')
+      console.log('toggleloaded called');
       this.setState({
-        loading: false, 
+        loading: false,
         noQuizzes: true,
-      })
+      });
     }
   }
 
@@ -256,14 +258,22 @@ class QuizArchiveBase extends Component {
   }
 
   render() {
-    console.log(this.state.loading, 'loading')
-    console.log(this.state,'state')
+    console.log(this.state.loading, 'loading');
+    console.log(this.state, 'state');
     const {
-      dateArray,
-      titleArray,
       rowsPerPage,
       page,
     } = this.state;
+
+    let dateArray = [];
+    let titleArray = [];
+
+    if (this.state.dateArray && this.state.dateArray.length > 0) {
+      dateArray = this.state.dateArray;
+      titleArray = this.state.titleArray;
+    }
+
+    console.log(dateArray, 'dateArray in render')
     const emptyRows = rowsPerPage - Math.min(rowsPerPage, dateArray.length - page * rowsPerPage);
 
     const List = dateArray.slice(page
@@ -303,7 +313,7 @@ class QuizArchiveBase extends Component {
           </div>
         );
       }
-      if (this.state.dateArray.length === 0 && this.state.selectedMonth === moment().startOf('month').format('YYYY-MM-DDTHH:mm')) {
+      if (this.props.quizzes.length === 0 && this.state.selectedMonth === moment().startOf('month').format('YYYY-MM-DDTHH:mm')) {
         return (
           <>
             <p>There are no quizzes available for this month yet! Check back later to play or
@@ -320,81 +330,79 @@ class QuizArchiveBase extends Component {
                   <OutlinedInput name="month" fullWidth id={'selectedMonth'} style={{ height: '6vh' }} labelWidth={0}/>
                 }
               >
-                {this.state.monthsArray && this.state.monthsArray.map((month, i) => (
+                {this.state.monthsArray && this.state.monthsArray.map((month) => (
                   <option value={month} key={month}>{moment(month).format('MMMM')}</option>
                 ))}
               </Select>
               <Button className="olderButton" variant="contained" color="primary" onClick={this.getQuizzesFromDb}>View</Button>
             </div>
           </>
-        )
-      } else {
-        return (
-          <div>
-            <Table className="archive-table">
-              <TableHead>
-                <TableRow>
-                  <TableCell style={{ minWidth: '66px'}} padding="default">
-                    Quiz Date
-                  </TableCell>
-                  <TableCell style={{ minWidth: '85px'}} padding="none">
-                    Quiz Title
-                  </TableCell>
-                  <TableCell style={{ minWidth: '40px'}} padding="default">
-                    Your Score
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {List}
-                {emptyRows > 0 && (
-                  <TableRow style={{ height: 48 * emptyRows }}>
-                    <TableCell colSpan={6} />
-                  </TableRow>
-                )}
-              </TableBody>
-              <TableFooter>
-                <TableRow>
-                  <TablePaginationActions
-                    rowsPerPageOptions={[ 5, 10, 25]}
-                    colSpan={3}
-                    count={dateArray.length === undefined ?  0 : dateArray.length }
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    selectprops={{
-                      native: true
-                    }}
-                    onChangePage={this.handleChangePage}
-                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
-                  />
-                </TableRow>
-
-              </TableFooter>
-
-            </Table>
-            { Object.keys(this.state.scoreObject).length !== this.state.dateArray.length ? null : <p className="archive-warning">You have taken all of the available quizzes. Check back tomorrow for the latest challenge!</p>}
-            <hr style={{ marginBottom: '5vh', marginTop: '3vh'}} />
-            <div className="viewOlder">
-              <p className="olderDescription">View quizzes from previous months: </p>
-              <Select
-                native
-                value={this.state.selectedMonth}
-                onChange={event => this.handleChangeMonth(event.target.value)}
-                style={{ height: '6vh'}}
-                input={
-                  <OutlinedInput name="month" fullWidth id={"selectedMonth"} style={{ height: '6vh' }} labelWidth={0}/>
-                }
-              >
-                {this.state.monthsArray && this.state.monthsArray.map((month, i) => (
-                  <option value={month} key={month}>{moment(month).format('MMMM')}</option>
-                ))}
-              </Select>
-              <Button className="olderButton" variant="contained" color="primary" onClick={this.getQuizzesFromDb}>View</Button>
-            </div>
-          </div>
-        )
+        );
       }
-    }
+
+      return (
+        <div>
+          <Table className="archive-table">
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ minWidth: '66px' }} padding="default">
+                  Quiz Date
+                </TableCell>
+                <TableCell style={{ minWidth: '85px' }} padding="none">
+                  Quiz Title
+                </TableCell>
+                <TableCell style={{ minWidth: '40px' }} padding="default">
+                  Your Score
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {List}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 48 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePaginationActions
+                  rowsPerPageOptions={[5, 10, 25]}
+                  colSpan={3}
+                  count={dateArray.length === undefined ? 0 : dateArray.length }
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  selectprops={{
+                    native: true,
+                  }}
+                  onChangePage={this.handleChangePage}
+                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+          { Object.keys(this.state.scoreObject).length !== this.state.dateArray.length ? null : <p className="archive-warning">You have taken all of the available quizzes. Check back tomorrow for the latest challenge!</p>}
+          <hr style={{ marginBottom: '5vh', marginTop: '3vh' }} />
+          <div className="viewOlder">
+            <p className="olderDescription">View quizzes from previous months: </p>
+            <Select
+              native
+              value={this.state.selectedMonth}
+              onChange={(event) => this.handleChangeMonth(event.target.value)}
+              style={{ height: '6vh' }}
+              input={
+                <OutlinedInput name="month" fullWidth id={'selectedMonth'} style={{ height: '6vh' }} labelWidth={0}/>
+              }
+            >
+              {this.state.monthsArray && this.state.monthsArray.map((month) => (
+                <option value={month} key={month}>{moment(month).format('MMMM')}</option>
+              ))}
+            </Select>
+            <Button className="olderButton" variant="contained" color="primary" onClick={this.getQuizzesFromDb}>View</Button>
+          </div>
+        </div>
+      );
+    };
 
     return (
       <Paper className="home archive-holder">
@@ -406,8 +414,8 @@ class QuizArchiveBase extends Component {
           <Link to={routes.LEADERBOARD} style={{ textDecoration: 'none' }}><Button variant="contained" color="primary">Leaderboard</Button></Link>
         </div>
         <div className="mobile-archive-header">
-          <Link to={routes.HOME} style={{ textDecoration: 'none'}} className="mobile-back"><Button color="primary">Back</Button></Link>
-          <h1 style={{ display: 'inline'}}>{moment(this.state.displayMonth).format('MMMM')} Quizzes</h1>
+          <Link to={routes.HOME} style={{ textDecoration: 'none' }} className="mobile-back"><Button color="primary">Back</Button></Link>
+          <h1 style={{ display: 'inline' }}>{moment(this.state.displayMonth).format('MMMM')} Quizzes</h1>
           {moment().format('MMMM') !== moment(this.state.selectedMonth).format('MMMM')
             ? <p id="archive-disclaimer">You may take past quizzes to boost your politIQ, but they will not affect your rankings for this month.</p>
             : null
@@ -422,35 +430,41 @@ class QuizArchiveBase extends Component {
           className="archive-modal"
         >
           <Paper>
-            <Close style={{ float: 'right', padding: '1vh', display: 'block'}} onClick={this.toggleModal}/>
+            <Close style={{ float: 'right', padding: '1vh', display: 'block' }} onClick={this.toggleModal}/>
             <h3>Nice try!</h3>
-            <p>You've already taken this quiz! We can't let you view it again while this month's contest is ongoing. You'll be able to review the quiz (and the correct answers!) next month!</p>
+            <p>You&#39;ve already taken this quiz! We can&#39;t let you view it again while this
+               month&#39;s contest is ongoing. You&#39;ll be able to review the quiz (and the
+               correct answers!) next month!</p>
           </Paper>
         </Modal>
       </Paper>
-    )
+    );
   }
 }
 
 QuizArchiveBase.propTypes = {
   authUser: PropTypes.object,
   history: PropTypes.object.isRequired,
+  quizzes: PropTypes.array.isRequired,
 };
 
 
-const QuizArchive = ({ history }) => {
-  return (
-    <AuthUserContext.Consumer>
-      {(authUser) => (
-        <QuizContext.Consumer>
-          {(quizzes) => (
-            <QuizArchiveBase authUser={authUser} history={history} quizzes={quizzes}/>
-          )}
-        </QuizContext.Consumer>
-      )}
-    </AuthUserContext.Consumer>
-  )
-}
+const QuizArchive = ({ history }) => (
+  <AuthUserContext.Consumer>
+    {(authUser) => (
+      <QuizContext.Consumer>
+        {(quizzes) => (
+          <QuizArchiveBase authUser={authUser} history={history} quizzes={quizzes}/>
+        )}
+      </QuizContext.Consumer>
+    )}
+  </AuthUserContext.Consumer>
+);
+
+
+QuizArchive.propTypes = {
+  history: PropTypes.object.isRequired,
+};
 
 
 export default withRouter(QuizArchive);
