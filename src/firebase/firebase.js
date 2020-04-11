660cce01d351de990d25c25c05c222105cf6e8a4
Conner Shoop <connershoop@gmail.com>
2020-04-11 12:00:14 -0700

    move to env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,39 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+import "firebase/messaging";
+import "firebase/functions";
+
+const config = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId
+};
+
+let app;
+if (!firebase.apps.length) {
+  app = firebase.initializeApp(config);
+}
+
+const db = firebase.database();
+const auth = firebase.auth();
+const storage = firebase.storage();
+const functions = firebase.functions();
+
+let messaging;
+if (firebase.messaging.isSupported()) {
+  messaging = firebase.messaging();
+  messaging.usePublicVapidKey(
+    process.env.REACT_APP_MESSAGING_KEY
+  );
+}
+
+// for facebook 
+const provider = new firebase.auth.FacebookAuthProvider();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+export { app, db, auth, storage, provider, messaging, functions, googleProvider };
